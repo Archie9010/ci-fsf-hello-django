73c81979130bb3c6826dbc6641eeb133e45f6c9a
Archie9010 <archie9010@protonmail.com>
2022-10-26 14:01:13 +0000

    prepared to deploy to heroku

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -11,4 +11,3 @@ class TestModels(TestCase):
     def test_item_string_menthod(self):
         item = Item.objects.create(name='Test Todo Item')
         self.assertEqual(str(item), 'Test Todo Item')
-    
--- todo/views.py
+++ todo/views.py
@@ -9,7 +9,7 @@ def get_todo_list(request):
     items = Item.objects.all()
     context = {
         'items': items
-    } 
+    }
     return render(request, 'todo/todo_list.html', context)
 
 
